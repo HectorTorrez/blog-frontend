b11c912f92fa63dffc2544d627580db2609b5832
Hector <torrezhectorb@gmail.com>
2023-10-13 10:50:49 -0600

    feat/ get blogs by id

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { getBlog } from '../services/blogServices'
+import { type Blog as blog } from '../types/blogsTypes'
+import { Navbar } from './Navbar'
 
 export const Blog = (): JSX.Element => {
-  const { blogId } = useParams()
-  console.log(blogId)
+  const [blog, setBlog] = useState<blog | undefined>()
+  const { blogId } = useParams<{ blogId: string }>()
+  const get = async (): Promise<void> => {
+    if (blogId != null) {
+      const data = await getBlog(blogId)
+      setBlog(data)
+    }
+  }
+  useEffect(() => {
+    void get()
+  }, [blogId])
   return (
-    <div>Blog</div>
+    <>
+    <Navbar/>
+    <section className='flex  flex-col items-center gap-y-10 dark:text-white dark:bg-gray-900 min-h-screen'>
+      <article className='text-center '>
+      <h2 className='font-bold text-6xl'>{blog?.title}</h2>
+      <p className='font-semibold text-xl text-gray-500'>{blog?.author}</p>
+      </article>
+      <article>
+        {blog?.blogText}
+      </article>
+    </section>
+    </>
   )
 }
--- src/services/blogServices.ts
+++ src/services/blogServices.ts
@@ -6,3 +6,8 @@ export const getBlogs = async (): Promise<Blog[]> => {
   const response = await fetch(`${BASE_URL}/api/blogs`)
   return await response.json()
 }
+
+export const getBlog = async (id: string): Promise<Blog> => {
+  const response = await fetch(`${BASE_URL}/api/blogs/${id}`)
+  return await response.json()
+}
