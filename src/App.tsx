079c44e6e468512d2a60889cc564517a61f8c8dd
Hector <torrezhectorb@gmail.com>
2023-10-13 10:50:36 -0600

    fix/ change styles

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ export const App = (): JSX.Element => {
   }, [])
 
   return (
-    <section className='dark:bg-black h-screen'>
+    <section className='dark:bg-black min-h-screen'>
       <Navbar/>
     <div className='flex justify-end w-[90%] m-auto max-w-screen-lg mt-[10px]'>
       <Link to='/create-blog'className="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900">Create a Blog</Link>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export const Navbar = (): JSX.Element => {
 
   return (
 
-<nav className="bg-white border-gray-200 dark:bg-gray-900">
+<nav className="bg-white border-b border-gray-200 dark:bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 
         <Link to='/' className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Blog</Link>
