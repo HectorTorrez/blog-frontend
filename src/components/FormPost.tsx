a82d47acc8a0bf26ab792e1620abd678a4d5cd96
Hector <torrezhectorb@gmail.com>
2023-10-14 20:46:27 -0600

    feat/ create blog

--- src/components/FormPost.tsx
+++ src/components/FormPost.tsx
@@ -1,29 +1,56 @@
+import { LoginContext } from '../context/LoginContext'
+import { useLoginContext } from '../hooks/useLoginContext'
+import { createBlog, setToken } from '../services/blogServices'
+import { type BlogFormValues } from '../types/blogsTypes'
 import { Navbar } from './Navbar'
+import { useState } from 'react'
 
 export const FormPost = (): JSX.Element => {
+  const [blog, setBlog] = useState<BlogFormValues>({
+    title: '',
+    author: '',
+    url: '',
+    blogText: ''
+  })
+
+  const { user } = useLoginContext(LoginContext)
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    setBlog({ ...blog, [event.target.name]: event.target.value })
+  }
+
+  const handlePostBlog = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault()
+    try {
+      setToken(user?.token as string)
+      await createBlog(blog)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <section className="bg-white dark:bg-gray-900 h-screen">
         <Navbar/>
     <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
       <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add a new blog</h2>
-      <form action="#">
+      <form onSubmit={handlePostBlog}>
           <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
               <div className="sm:col-span-2">
-                  <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-                  <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-600 focus:border-gray-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500" placeholder="Async/await JavaScript" required/>
+                  <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+                  <input onChange={handleChange} value={blog.title} type="text" name="title" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-600 focus:border-gray-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500" placeholder="Async/await JavaScript" required/>
               </div>
               <div className="w-full">
-                  <label htmlFor="brand" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Author</label>
-                  <input type="text" name="brand" id="brand" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-600 focus:border-gray-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500" placeholder="Hector" required/>
+                  <label htmlFor="author" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Author</label>
+                  <input onChange={handleChange} value={blog.author} type="text" name="author" id="author" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-600 focus:border-gray-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500" placeholder="Hector" required/>
               </div>
               <div className="w-full">
-                  <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">URL</label>
-                  <input type="number" name="price" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-600 focus:border-gray-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500" placeholder="www.google.com" required/>
+                  <label htmlFor="url" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">URL</label>
+                  <input onChange={handleChange} value={blog.url} type="text" name="url" id="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-600 focus:border-gray-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500" placeholder="www.google.com" required/>
               </div>
 
               <div className="sm:col-span-2">
-                  <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Body Blog</label>
-                  <textarea id="description" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500" placeholder="Your text here"></textarea>
+                  <label htmlFor="blogText" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Blog description</label>
+                  <textarea onChange={handleChange} value={blog.blogText} id="description" name='blogText' className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500" placeholder="Your text here"></textarea>
               </div>
           </div>
           <button type="submit" className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
--- src/services/blogServices.ts
+++ src/services/blogServices.ts
@@ -1,12 +1,12 @@
-import { type LoginCredentials, type Blog, type LoginWithToken } from '../types/blogsTypes'
+import { type LoginCredentials, type Blog, type LoginWithToken, type BlogFormValues } from '../types/blogsTypes'
 
 const BASE_URL = 'http://localhost:3003'
 
-// let token: string | null = null
+let token: string | null = null
 
-// export const setToken = (newToken: string): void => {
-//   token = `Bearer ${newToken}`
-// }
+export const setToken = (newToken: string): void => {
+  token = `bearer ${newToken}`
+}
 
 export const getBlogs = async (): Promise<Blog[]> => {
   const response = await fetch(`${BASE_URL}/api/blogs`)
@@ -29,17 +29,18 @@ export const login = async (credentials: LoginCredentials): Promise<LoginWithTok
   return await response.json()
 }
 
-// export const createBlog = async (credentials: LoginCredentials) => {
-//   if (token != null) {
-//     const response = await fetch(`${BASE_URL}/api/blogs`, {
-//       method: 'POST',
-//       headers: {
-//         Authorization: token
-//       },
-//       body: JSON.stringify(credentials)
+export const createBlog = async (blog: BlogFormValues) => {
+  if (token != null) {
+    const response = await fetch(`${BASE_URL}/api/blogs`, {
+      method: 'POST',
+      headers: {
+        Authorization: token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(blog)
 
-//     })
-//     console.log(response)
-//     return await response.json()
-//   }
-// }
+    })
+    console.log(response)
+    return await response.json()
+  }
+}
