2bfd0efc1ed5cff9e2abbb57372d2b8fcf9feccd
Hector <torrezhectorb@gmail.com>
2023-10-14 20:46:13 -0600

    feat/ get token

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from 'react'
 import { BlogCard } from './components/BlogCard'
 import { Navbar } from './components/Navbar'
-import { getBlogs } from './services/blogServices'
+import { getBlogs, setToken } from './services/blogServices'
 import { type Blog } from './types/blogsTypes'
 import { Link } from 'react-router-dom'
+import { LoginContext } from './context/LoginContext'
+import { useLoginContext } from './hooks/useLoginContext'
 
 export const App = (): JSX.Element => {
   const [blogs, setBlogs] = useState<Blog[]>([])
+
+  const { user } = useLoginContext(LoginContext)
+
   const fetchData = async (): Promise<void> => {
     try {
       const data = await getBlogs()
@@ -17,13 +22,25 @@ export const App = (): JSX.Element => {
   }
   useEffect(() => {
     void fetchData()
+    const token = localStorage.getItem('user')
+    if (JSON.parse(token as string) !== null) {
+      const user = JSON.parse(token as string).token
+      setToken(user.token)
+    }
   }, [])
 
   return (
     <section className='dark:bg-black min-h-screen'>
       <Navbar/>
     <div className='flex justify-end w-[90%] m-auto max-w-screen-lg mt-[10px]'>
+      {
+        user !== null
+          ? (
       <Link to='/create-blog'className="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900">Create a Blog</Link>
+
+            )
+          : null
+      }
     </div>
       <main className='max-w-screen-xl flex justify-center flex-wrap gap-10 items-center  mx-auto mt-10'>
         {blogs?.map((blog) => {
--- src/context/LoginContext.tsx
+++ src/context/LoginContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, type ReactNode, useState } from 'react'
+import { createContext, type ReactNode, useState, useEffect } from 'react'
 import { type LoginWithToken } from '../types/blogsTypes'
 
 interface LoginContextType {
@@ -17,8 +17,19 @@ export const LoginContext = createContext<LoginContextType>({
 
 export const LoginProvider = ({ children }: { children: ReactNode }): JSX.Element => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [user, setUser] = useState<LoginWithToken | null>(null)
-  console.log(user)
+  const [user, setUser] = useState<LoginWithToken | null>(
+    () => {
+      const user = localStorage.getItem('user')
+      if (user !== null) {
+        return JSON.parse(user)
+      }
+      return null
+    }
+  )
+
+  useEffect(() => {
+    localStorage.setItem('user', JSON.stringify(user))
+  }, [user])
 
   return (
         <LoginContext.Provider value={{ isLoggedIn, setIsLoggedIn, setUser, user }}>
