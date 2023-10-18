12b3d7cc7f95bbe1be087372ee1f4a14e0b1ea11
Hector <torrezhectorb@gmail.com>
2023-10-18 10:58:25 -0600

    fix/ error set incorrect item in localstorage

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -32,8 +32,10 @@ export const Login = (): JSX.Element => {
       if (getUser.error !== null) {
         setError(getUser.error)
       }
-      setUser(getUser)
-      setToken(getUser.token)
+      if (getUser.token.length > 0) {
+        setUser(getUser)
+        setToken(getUser.token)
+      }
       if (getUser.token?.length > 0) {
         navigate('/')
       }
@@ -49,7 +51,7 @@ export const Login = (): JSX.Element => {
         <div className="w-full bg-white mb-[150px] rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 ">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
       {
-        (error.length > 0) ? <Alert text={error} type='error'/> : null
+        (error?.length > 0) ? <Alert text={error} type='error'/> : null
       }
                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                     Login
