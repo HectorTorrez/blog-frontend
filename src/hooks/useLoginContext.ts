b0da576b01753116f83a92250579efdb2c276c21
Hector <torrezhectorb@gmail.com>
2023-10-27 21:15:17 -0600

    fix/ delete customHook and fix eslint

--- src/hooks/useLoginContext.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react'
-export const useLoginContext = (context: any): any => {
-  const contextValue = useContext(context)
-
-  if (contextValue === undefined) {
-    throw new Error('useLoginContext must be used within a LoginContextProvider')
-  }
-  return contextValue
-}
